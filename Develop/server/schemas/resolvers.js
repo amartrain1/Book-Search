e6887968fb5077d6315076a91d390ac5e3286db9
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-06-27 13:02:59 -0500

    added schema folder and created all the files in it

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,82 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    // get a single user by either their id or their username
+    getUser: async (_, { _id }) => {
+      try {
+        const user = await User.findById(_id);
+
+        if (!user) {
+          return res.status(400).json({ message: "User does not exist!" });
+        }
+
+        return user;
+      } catch (err) {
+        throw new Error("Failed to fetch user");
+      }
+    },
+  },
+  Mutation: {
+    createUser: async (_, { username, email, password }) => {
+      try {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        console.log(token);
+        return {
+          token,
+          user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            bookCount: user.bookCount,
+          },
+        };
+      } catch (err) {
+        throw new Error("Failed to create user");
+      }
+    },
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (_, { user, body }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return res.json(updatedUser);
+      } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+      }
+    },
+    deleteBook: async (_, { user, params }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res
+          .status(404)
+          .json({ message: "Couldn't find user with this id!" });
+      }
+      return res.json(updatedUser);
+    },
+  },
+};
+
+module.exports = resolvers;
